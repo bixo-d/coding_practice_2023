044ef86e7d235140103c8210f912d097aaabfce8
Rafael Aiquel Garbarini <rafael.aiquel@gmail.com>
2023-01-07 13:09:42 -0400

    El programa ahora calcula el RIF para persona natural venezolana

--- dayXXX/web/Calculo_digito_verificador_RIF/Calculo_digito_verificador_RIF.js
+++ dayXXX/web/Calculo_digito_verificador_RIF/Calculo_digito_verificador_RIF.js
@@ -7,24 +7,31 @@ btn.addEventListener("click", function () {
         alert("Please fill out the input fields!");
         return;
     }
-
-    let RIF = cid + 100000000;
+    console.log(typeof (parseInt(cid)));
+    let RIF = parseInt(cid) + 100000000;
+    console.log(RIF.toString().split(''));
 
     let factors = [4, 3, 2, 7, 6, 5, 4, 3, 2];
-    let testarr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    console.log(factors);
+    let testarr = RIF.toString().split('');
+    console.log(testarr);
 
     var step_two = 0;
-    
+
     for (var i = 0; i < factors.length; i++) {
-        step_two += (factors[i] * testarr[i]);
+        step_two += (factors[i] * parseInt(testarr[i]));
     }
-  
+
     step_three = step_two % 11;
-  
+
     codigo = 11 - step_three;
+
+    if (codigo >= 10) {
+        codigo = 0;
+    }
 
     console.log(codigo);
     //alert(step_two);
 
-    document.querySelector("#result").innerHTML = RIF;
+    document.querySelector("#result").innerHTML = cid + "-" + codigo;
 });
